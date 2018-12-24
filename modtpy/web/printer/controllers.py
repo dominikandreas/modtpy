26f7e44e145d9216e9a9a25346933987c361b089
dominikandreas <13525040+dominikandreas@users.noreply.github.com>
2018-12-24 13:01:25 +0100

    pep8

--- modtpy/web/printer/controllers.py
+++ modtpy/web/printer/controllers.py
@@ -5,25 +5,29 @@ from modtpy.api import ModT
 
 printer = Blueprint('printer', __name__)
 
+
 @printer.before_request
 def before_request():
     if hasattr(g, 'modt'):
         return
     try:
         g.modt = ModT()
-        g.modt.dev # TODO: some better way to check connectivity upfront
+        g.modt.dev  # TODO: some better way to check connectivity upfront
     except (ValueError, usb.core.NoBackendError) as error:
         raise USBError(str(error), status_code=503)
+
 
 @printer.route('/status')
 def status():
     status = g.modt.get_status()
     return jsonify(status=status)
 
+
 @printer.route('/load-filament')
 def load_filament():
     g.modt.load_filament()
     return jsonify()
+
 
 @printer.route('/unload-filament')
 def unload_filament():
